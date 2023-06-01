1fe5ee4f06085b37cb905bd973f7d0472ee25661
Lukas Schramm <lukas@dorpapst.de>
2023-06-01 18:29:54 +0200

    Add stations and transformation for URL

--- Enturinator.py
+++ Enturinator.py
@@ -1,39 +1,20 @@
-# URL-Example
-''' 
-https://entur.no/reiseresultater?
-transportModes=rail%2Ctram%2Cbus%2Ccoach%2Cwater%2Ccar_ferry%2Cmetro%2Cflytog%2Cflybuss
-&date=1685955600000
-&tripMode=oneway
-&walkSpeed=1.3
-&minimumTransferTime=120
-&timepickerMode=departAfter
-&startId=NSR%3AStopPlace%3A59872
-&startLabel=Oslo%20S
-&startLat=59.910357
-&startLon=10.753051
-&stopId=NSR%3AStopPlace%3A59983
-&stopLabel=Bergen%20stasjon
-&stopLat=60.390434
-&stopLon=5.333511
-
-'''
-
 from bs4 import BeautifulSoup
 import requests
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import datetime
+import pytz
 
 def fetch_website(url):
-    options = Options()
-    options.add_argument("--headless")  # Run Chrome in headless mode
-    driver = webdriver.Chrome(options=options)
-    driver.get(url)
-    time.sleep(10)  # Wait for 10 seconds
-    content = driver.page_source
-    driver.quit()
-    return content
+	options = Options()
+	options.add_argument("--headless")  # Run Chrome in headless mode
+	driver = webdriver.Chrome(options=options)
+	driver.get(url)
+	time.sleep(10)  # Wait for 10 seconds
+	content = driver.page_source
+	driver.quit()
+	return content
 
 def get_transit_container(html_content):
 	soup = BeautifulSoup(html_content, 'html.parser')
@@ -50,20 +31,60 @@ def read_html_file(file_path):
 		content = file.read()
 	return content
 
+def generate_url(date, station_from, station_to):
+	url = f"https://entur.no/reiseresultater?transportModes=rail%2Ctram%2Cbus%2Ccoach%2Cwater%2Ccar_ferry%2Cmetro%2Cflytog%2Cflybuss"
+	url += f"&date={date}000"	# travel day
+	url += f"&tripMode=oneway"
+	url += f"&walkSpeed=1.3"
+	url += f"&minimumTransferTime=120"
+	url += f"&timepickerMode=departAfter"
+	url += f"&startId={station_from[0]}"		# start
+	url += f"&startLabel={station_from[1]}"
+	url += f"&startLat={station_from[2]}"
+	url += f"&startLon={station_from[3]}"
+	url += f"&stopId={station_to[0]}"			# end
+	url += f"&stopLabel={station_to[1]}"
+	url += f"&stopLat={station_to[2]}"
+	url += f"&stopLon={station_to[3]}"
+	return url
+
+def read_stations(file_path):
+	data_dict = {}
+	with open(file_path, "r") as file:
+		lines = file.readlines()
+		header = lines[0].strip().split(',') 
+		for line in lines[1:]:
+			values = line.strip().split(',')
+			common_name = values[0].replace("\"","")
+			rest_of_values = tuple([a.replace("\"","") for a in values[1:]])
+			data_dict[common_name] = rest_of_values
+	return data_dict
+
+def convert_to_unix_time(year, month, day, hour, minute):
+	oslo_timezone = pytz.timezone('Europe/Oslo')
+	oslo_datetime = oslo_timezone.localize(datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute))
+	unix_time = int(oslo_datetime.timestamp())
+	return unix_time
+
 def main():
+	# Parameter
 	debug = True
+
+	# Stations
+	station_dict = read_stations("data/stations.txt")
 	
 	if debug:
 		content = read_html_file("./debug/example_html.txt")
-		print(content)
 	else:
-		url = f"https://entur.no/reiseresultater?transportModes=rail%2Ctram%2Cbus%2Ccoach%2Cwater%2Ccar_ferry%2Cmetro%2Cflytog%2Cflybuss&date=1685955600000&tripMode=oneway&walkSpeed=1.3&minimumTransferTime=120&timepickerMode=departAfter&startId=NSR%3AStopPlace%3A59872&startLabel=Oslo%20S&startLat=59.910357&startLon=10.753051&stopId=NSR%3AStopPlace%3A59983&stopLabel=Bergen%20stasjon&stopLat=60.390434&stopLon=5.333511"
-
+		url = generate_url(date = convert_to_unix_time(2023, 7, 13, 18, 0), station_from=station_dict["Bergen"], station_to=station_dict["Myrdal"])
 		content = fetch_website(url)
 
+	# Mine content
 	transit_data = get_transit_container(content)
 	prettified_content = transit_data.prettify()
 	print(prettified_content)
+
+	
 
 if __name__ == "__main__":
 	main()
--- README.md
+++ README.md
@@ -7,3 +7,13 @@ This project aims to generate a pricing calendar for Norwegian train connections
 * customise known routes
 * set standard stations (like Bergen Jernbanestasjon)
 * notification
+
+## Things to fix
+* relies on Google Chrome, maybe find something else (Raspberry Pi friendly)
+* Entur relies heavily on a weird URL format which requires for parameter for each station ([label, longitude, latitude, stopID]). Since three of them aren't obvious, there is a dictionary file in ``data/stations.txt`` which has some common stations in it. If yours is missing, add a new line in there
+
+## Run
+```python3 Enturinator.py```
+
+## Debug
+There is an example file in the debug folder. Activate ``debug=true`` in the main function such that it is not necessary to download the data every time you test something. This may also prevent you from being IP banned
